feba77b108e96551980a7eb3bc447b8c3a9c4e53
SanskarP1 <sanskarpokharel@gmail.com>
2023-03-03 14:08:34 +0545

    provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
 class HomePage extends ConsumerWidget {
   const HomePage({super.key});
 
